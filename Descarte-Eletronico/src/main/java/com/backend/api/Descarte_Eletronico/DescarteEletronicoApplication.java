c632afd49d037409a8d3ac8ee5a28d9e8e038cd0
Rafael Silva Martins <rafael.sm008@gmail.com>
2026-05-01 19:48:14 -0300

    initial-commit

---
+++ Descarte-Eletronico/src/main/java/com/backend/api/Descarte_Eletronico/DescarteEletronicoApplication.java
@@ -0,0 +1,13 @@
+package com.backend.api.Descarte_Eletronico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DescarteEletronicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DescarteEletronicoApplication.class, args);
+	}
+
+}
---
+++ Descarte-Eletronico/src/test/java/com/backend/api/Descarte_Eletronico/DescarteEletronicoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.api.Descarte_Eletronico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DescarteEletronicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
