ab70eb6476831db8d580b9dbe2aab9fab6b85ef7
Rafael Silva Martins <rafael.sm008@gmail.com>
2026-05-11 21:02:05 -0300

    Rename column 'senha_hash' to 'senha' in usuarios_admin

--- Descarte-Eletronico/src/main/resources/db/migration/V2__create_usuarios_admin.sql
+++ Descarte-Eletronico/src/main/resources/db/migration/V2__create_usuarios_admin.sql
@@ -7,5 +7,5 @@ CREATE TABLE IF NOT EXISTS usuarios_admin (
     deleted_at TIMESTAMP,
     nome VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL,
-    senha_hash VARCHAR(255) NOT NULL
+    senha VARCHAR(255) NOT NULL
 );
