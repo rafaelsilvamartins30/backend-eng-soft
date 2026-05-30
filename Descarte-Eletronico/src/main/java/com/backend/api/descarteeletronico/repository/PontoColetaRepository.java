4c5c5d0a88ad9a2338c1df23f57374fa966dba2d
Ezequiel Dominguez Santos <eze.dmgz06@gmail.com>
2026-05-30 20:45:57 -0300

    add: paginacao para listagem de pontos de coleta

--- Descarte-Eletronico/src/main/java/com/backend/api/descarteeletronico/repository/PontoColetaRepository.java
+++ Descarte-Eletronico/src/main/java/com/backend/api/descarteeletronico/repository/PontoColetaRepository.java
@@ -5,6 +5,9 @@ import com.backend.api.descarteeletronico.model.pontocoleta.PontoColeta;
 import java.util.Optional;
 import java.util.Set;
 import java.util.UUID;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -15,4 +18,7 @@ public interface PontoColetaRepository extends JpaRepository<PontoColeta, UUID> 
 
   @EntityGraph(attributePaths = "tiposProduto")
   Set<PontoColeta> findAllByEntityStatus(EntityStatus entityStatus);
+
+  @EntityGraph(attributePaths = "tiposProduto")
+  Page<PontoColeta> findAllByEntityStatus(EntityStatus entityStatus, Pageable pageable);
 }
